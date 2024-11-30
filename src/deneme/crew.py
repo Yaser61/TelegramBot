b515f96b2dc6266134c41d4810b150020c919532
Şafak Saylam <safaksaylamm@gmail.com>
2024-12-01 00:37:36 +0300

    Add environment handling and LLM integration
    Incorporated dotenv for environment variable management and added LLM integration for crew agents. Updated `.gitignore` for new files and included dependencies in `requirements.txt` for easier setup.

--- src/deneme/crew.py
+++ src/deneme/crew.py
@@ -1,11 +1,24 @@
-from crewai import Agent, Crew, Process, Task
+from crewai import Agent, Crew, Process, Task, LLM
 from crewai.project import CrewBase, agent, crew, task, before_kickoff, after_kickoff
+from dotenv import load_dotenv
+import os
 
 # Uncomment the following line to use an example of a custom tool
 # from deneme.tools.custom_tool import MyCustomTool
 
 # Check our tools documentations for more information on how to use them
 # from crewai_tools import SerperDevTool
+
+def llm():
+	env_path = os.path.join(os.path.dirname(__file__), '.env')
+	load_dotenv(dotenv_path=env_path)
+
+	return LLM(
+		model=os.environ.get("AZURE_API_MODEL"),
+		api_key=os.environ.get("AZURE_API_KEY"),  # Replace with KEY1 or KEY2
+		base_url=os.environ.get("AZURE_API_BASE"),  # example: https://example.openai.azure.com/
+		api_version=os.environ.get("AZURE_API_VERSION"),  # example: 2024-08-01-preview
+	)
 
 @CrewBase
 class Deneme():
@@ -28,17 +41,21 @@ class Deneme():
 
 	@agent
 	def researcher(self) -> Agent:
+		print(os.environ.values())
+		print("naber")
 		return Agent(
 			config=self.agents_config['researcher'],
+			llm=llm(),
 			# tools=[MyCustomTool()], # Example of custom tool, loaded on the beginning of file
-			verbose=True
+			#verbose=True
 		)
 
 	@agent
 	def reporting_analyst(self) -> Agent:
 		return Agent(
 			config=self.agents_config['reporting_analyst'],
-			verbose=True
+			llm=llm(),
+			#verbose=True
 		)
 
 	@task
